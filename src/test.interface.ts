203e6122de87ff24865482b47e241518c7691472
perosky168 <ogunbanjopetet10@gmail.com>
2025-04-12 19:08:07 +0100

    change repo to public

---
+++ src/test.interface.ts
@@ -0,0 +1,58 @@
+import { ObjectId } from 'mongoose';
+import { IRatingCategories } from './review.interface';
+
+// By extending ISellerDocument with the Record<string, any> you allow an object to contain other
+// string keys with any values along with those defined in the interface.
+// The nice part is that you still have the autocompletion for the defined properties
+export type SellerType =
+  | string
+  | string[]
+  | number
+  | IRatingCategories
+  | Date
+  | IExperience
+  | IExperience[]
+  | IEducation
+  | IEducation[]
+  | ICertificate
+  | ICertificate[]
+  | ILanguage
+  | ILanguage[]
+  | unknown
+  | undefined;
+
+export interface ILanguage {
+  [key: string]: string | number | undefined;
+  _id?: string;
+  language: string;
+  level: string;
+}
+
+export interface IExperience {
+  [key: string]: string | number | boolean | undefined;
+  _id?: string;
+  company: string;
+  title: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  currentlyWorkingHere: boolean | undefined;
+}
+
+export interface IEducation {
+  [key: string]: string | number | undefined;
+  _id?: string;
+  country: string;
+  university: string;
+  title: string;
+  major: string;
+  year: string;
+}
+
+export interface ICertificate {
+  [key: string]: string | number | undefined;
+  _id?: string;
+  name: string;
+  from: string;
+  year: number | string;
+}
